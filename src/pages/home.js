1e54e7868ad50c80b3a5b2d0e96e4d6ebcc949e5
Tiago Rosa <tiago.rosa@clamed.com.br>
2020-11-11 13:14:34 -0300

    note e noteFeed e estilização da Home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import Button from '../components/Button';
+import { useQuery, gql } from '@apollo/client';
+import ReactMarkdown from 'react-markdown';
+import NoteFeed from '../components/NoteFeed';
+
+const GET_NOTES = gql`
+  query noteFeed($cursor: String){
+  noteFeed(cursor: $cursor){
+    cursor
+    hasNextPage
+    notes{
+      id
+      createdAt
+      content
+      favoriteCount
+      author{
+        username
+        id
+        avatar
+      }
+    }
+  }
+}
+`;
+
 const Home = ()=>{
-  return(
-    <div>
-     <p>This is the home page</p>
-     <Button>Click me!</Button>
-    </div>
-  );
+  
+  const {data,loading,error,fetchMore} = useQuery(GET_NOTES);
+
+  if(loading) return <p>Loading...</p>;
+  if(error) return <p>Error!</p>;
+
+  return <NoteFeed notes={data.noteFeed.notes} />;  
 };
 export default Home;
